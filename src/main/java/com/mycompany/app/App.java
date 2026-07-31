3d93005fc23d5081f04cb9baa1683e36dd6c4a87
Super User <root@ip-172-31-12-159.ap-south-1.compute.internal>
2026-07-31 07:06:08 +0000

    Add App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,7 +1,15 @@
-package com.example;
+package com.mycompany.app;
 
 public class App {
+
+    public App() {
+    }
+
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello World!");
     }
 }
